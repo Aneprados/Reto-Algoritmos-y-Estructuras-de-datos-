7348ee1b59829f6df7af1b990a65a7c672896c31
Aneprados <apradoscamara@gmail.com>
2025-04-08 16:28:29 +0100

    ponemos las carpetas de caballo y reina

--- reina/reinas.py
+++ reina/reinas.py
@@ -1,7 +1,7 @@
-class Caballos:
+class Reinas:
     def __init__(self, origen, destino):
         self.origen = origen  # Posición inicial (tupla: fila, columna)
         self.destino = destino  # Posición final (tupla: fila, columna)
 
     def __repr__(self):
-        return f"Caballo(origen={self.origen}, destino={self.destino})"
+        return f"Reina(origen={self.origen}, destino={self.destino})"
